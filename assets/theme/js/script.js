53d1c5394bded9242c45cd5c349d4ccc2c218dc0
IEBVG <iebvg22@gmail.com>
2024-06-23 20:18:13 -0300

    Update script.js

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,6 +1,6 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
-const textArray = ["familia", "iglesia","a la casa del Rey de Reyes"];
+const textArray = ["familia", "iglesia","a la casa de Dios"];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 2000; // Delay between current and next text
